ba7e3212ffa3de94a529a616f171eefb1430bfbd
Dmitriy Mekhov <mekhov@kodeks.ru>
2017-10-05 11:18:45 +0300

    Изменена папка отслеживания env файла
    указана принудительно /var/www - ранее файл отслеживался в той же папке где находится галп файл

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var watch = require('gulp-watch');
 
 
 gulp.task('make_local_env', function() {
-    watch('.env.*', function() {
+    gulp_run('ls -la').exec();
+    gulp_run('ls /var/www/ -la').exec();
+    watch('/var/www/.env.*', function() {
         gulp_run('php /var/utils/env.php -e local').exec();
     });
 });
